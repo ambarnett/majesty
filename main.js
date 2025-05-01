79911a8fd9d7b5ee2a454e339c6c02b8c2e288da
Andy <andy.barnett09@gmail.com>
2025-05-01 10:08:09 -0500

    created data.js file and updated functionality

--- main.js
+++ main.js
@@ -2,8 +2,7 @@
     Define a function with a single parameter. When the function
     is invoked, an argument is required. 
 */
-const tributeChest = []
-const queens = []
+const {database} = require("./data.js")
 
 const createQueen = (queenId, queenName) => {
     const queenObject = {
@@ -11,7 +10,7 @@ const createQueen = (queenId, queenName) => {
         name: queenName
     }
 
-    queens.push(queenObject)
+    database.queens.push(queenObject)
 }
 
 const payTribute = (tributeId, tributeDiscription, queenId) => {
@@ -21,7 +20,7 @@ const payTribute = (tributeId, tributeDiscription, queenId) => {
         queenId: queenId
     }
 
-    tributeChest.push(tributeObject)
+    database.tributeChest.push(tributeObject)
 }
 
 createQueen(1, "Trinity Terry")
@@ -35,15 +34,15 @@ const hailTheQueen = (nameString) => {
     return `hail her majesty, ${nameString}.` //This function returns a string
 }
 
-for (const queen of queens) {
+for (const queen of database.queens) {
     
     const hailMessage = hailTheQueen(queen.name) //What should be the argument?
     console.log(hailMessage)
 }
 
 // interate the queen objects to show tribute for each one
-for (const queen of queens) {
-    for (const tribute of tributeChest) {
+for (const queen of database.queens) {
+    for (const tribute of database.tributeChest) {
         if (tribute.queenId === queen.id) {
             
             console.log(`${queen.name} has tribute ${tribute.description}`)
